4b5574a363b00bce165e10e20eea7ce94016e9a3
Leechuk <55555794+Leechuk@users.noreply.github.com>
2019-09-20 13:56:01 +0200

    Sesion 20/09/2019

---
+++ Assets/_GameObjects/Scripts/Pajaro.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Pajaro : MonoBehaviour
+{
+    Rigidbody rb;
+    int fuerza = 700;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+            
+    }
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Saltar();
+            
+        }
+       
+
+    }
+
+    void Saltar()
+    {
+        rb.AddForce(Vector3.up * fuerza);
+    }
+}
---
+++ Assets/_GameObjects/Scripts/PipesSpamer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipesSpamer : MonoBehaviour
+{
+    public float ratio;
+
+    public GameObject prefabPipe;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnPipe", 0, ratio); //empezando de inmediato, Cada ratio en segundos 
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void SpawnPipe()
+    {
+        Instantiate(prefabPipe, transform);
+        
+    }
+}

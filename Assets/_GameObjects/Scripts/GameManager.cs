e528576dd909ab0e3e75e873a6d63a5d9c000db8
Leechuk <55555794+Leechuk@users.noreply.github.com>
2019-09-23 14:05:23 +0200

    Update 23-09-2019

---
+++ Assets/_GameObjects/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //metodo para cargar libreria para gestionar escena
+
+
+public class GameManager : MonoBehaviour
+{
+    
+    public static void Reload()
+    {
+        SceneManager.LoadScene(0);// recarga de la escena
+    }
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/_GameObjects/Scripts/Pajaro.cs
+++ Assets/_GameObjects/Scripts/Pajaro.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 
 public class Pajaro : MonoBehaviour
 {
+    public int puntuacion = 0;
     Rigidbody rb;
-    int fuerza = 700;
+    public int fuerza = 650;
+    public Text txtPuntuacion;
+    [SerializeField] GameObject prefabSangre; // Sistema de particulas de explosion del pollo 
+    //SerialzeField permite modificacion en el editor
 
     // Start is called before the first frame update
     void Start()
@@ -32,4 +37,34 @@ public class Pajaro : MonoBehaviour
     {
         rb.AddForce(Vector3.up * fuerza);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        Morir();
+
+    }
+
+    private void Morir()
+    {
+        Instantiate(prefabSangre, transform.position, transform.rotation); //Instaciamos la sangre y aplicamos el transform
+        Destroy(gameObject); // Se destruye el personaje al chocar con las tuberias
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Limite") == true)
+        {
+            Morir();
+        }
+        else
+        {
+            puntuacion++;
+            txtPuntuacion.text = puntuacion.ToString();
+        }
+        
+       
+    }
+
 }
+
